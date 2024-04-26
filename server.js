8e65e497b4b492f80dda83e3df1959941b2586c3
itxFox <145446150+itxFox@users.noreply.github.com>
2024-04-26 20:29:25 +0200

    primo commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const fs = require('fs').promises; //promises per funzioni asincrone
+const app = express();
+
+async function caricaDati(path) {
+  try {
+    const data = await fs.readFile(path, 'utf8');
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(`Errore durante il caricamento del file ${path}:`, error);
+    return null;
+  }
+}
+
+// Rotte
+app.get('/mafie', async (req, res) => {
+  const mafie = await caricaDati('./public/json/mafie.json');
+  if (mafie) {
+    res.json(mafie);
+  } else {
+    res.status(500).json({ error: 'Errore durante il caricamento dei dati delle mafie' });
+  }
+});
+
+app.get('/dettagli', async (req, res) => {
+  const dettagli = await caricaDati('./public/json/dettagli.json');
+  if (dettagli) {
+    res.json(dettagli);
+  } else {
+    res.status(500).json({ error: 'Errore durante il caricamento dei dettagli delle organizzazioni' });
+  }
+});
+
+app.get('/attentati', async (req, res) => {
+  const attentati = await caricaDati('./public/json/attentati.json');
+  if (attentati) {
+    res.json(attentati);
+  } else {
+    res.status(500).json({ error: 'Errore durante il caricamento dei dati degli attentati' });
+  }
+});
+
+// Avvio del server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server in ascolto sulla porta ${port}`);
+});
